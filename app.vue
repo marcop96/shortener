128c25a127eb2caebd1cc99d77be91d3dfe61e20
Marco <me@marcopianaroli.com>
2024-05-21 19:10:17 +0200

    feat:tests fetched data

--- app.vue
+++ app.vue
@@ -1,5 +1,18 @@
+<script setup lang="ts">
+const client = useSupabaseClient();
+const shortened_urls = ref();
+
+async function getShortenedUrls() {
+  const { data } = await client.from("shortened_urls").select();
+
+  shortened_urls.value = data;
+}
+
+onMounted(() => {
+  getShortenedUrls();
+});
+</script>
+
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+  <pre>{{ shortened_urls }}</pre>
 </template>
