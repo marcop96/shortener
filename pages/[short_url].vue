496e7eeaec1c14429896940a8f98f2d9399e870e
Marco Pianaroli <marco.pianaroli96@gmail.com>
2024-05-24 15:04:45 +0200

    feat:  dynamically redirects to the correct site

--- pages/[short_url].vue
+++ pages/[short_url].vue
@@ -1,10 +1,40 @@
 <script setup lang="ts">
-onBeforeMount(() => {
-  navigateTo('https://google.com',{external: true});
-});
 
+
+const client = useSupabaseClient();
+const route = useRoute();
+const path = ref<string>('');
+
+async function getLongUrl() {
+  try {
+    const { data, error } = await client
+      .from('shortened_urls')
+      .select()
+      .eq('short_url', route.params.short_url);
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    if (data && data.length > 0) {
+      path.value = data[0].long_url;
+    } else {
+      console.error('No data found');
+    }
+  } catch (error) {
+    console.error('Error fetching long URL:', error);
+  }
+}
+
+onMounted(async () => {
+  await getLongUrl();
+  console.log(path.value);
+  if (path.value) {
+    window.location.replace(path.value);
+  }
+});
 </script>
 
 <template>
-
-</template>
+LOADING</template>
