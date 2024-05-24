4c9f743186cd7d080c3d720621191b7763071e0b
Marco Pianaroli <marco.pianaroli96@gmail.com>
2024-05-24 14:36:28 +0200

    feat:redirects to test external site

--- pages/[short_url].vue
+++ pages/[short_url].vue
@@ -1,11 +1,10 @@
 <script setup lang="ts">
-import { ref } from "vue";
-const route = useRoute();
-const hello = ref("hello world");
+onBeforeMount(() => {
+  navigateTo('https://google.com',{external: true});
+});
 
-console.log(route.params.short_url);
 </script>
 
 <template>
-  {{ hello }}
+
 </template>
