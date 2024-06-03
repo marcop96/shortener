7f1a375600c9746cb7a8daf95d82cdeb3cecb741
Marco <me@marcopianaroli.com>
2024-06-03 20:06:03 +0200

    updates dependencies

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxtjs/supabase", "@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  modules: [
+    "@nuxtjs/supabase",
+    "@nuxtjs/tailwindcss",
+    "shadcn-nuxt",
+    "radix-vue/nuxt",
+  ],
 });
