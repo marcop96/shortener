3ee9e218b4224384e4fe5b1fa92031f559b1a752
Marco Pianaroli <marco.pianaroli96@gmail.com>
2024-06-07 09:14:22 +0200

    refactor: login form

--- components/LoginSite/LoginForm.vue
+++ components/LoginSite/LoginForm.vue
@@ -1,60 +1,80 @@
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
- import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import { Input } from '@/components/ui/input'
-import { Label } from '@/components/ui/label'
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+const supabase = useSupabaseClient();
+const loginEmail = ref("");
+const loginPassword = ref("");
+
+async function loginHandler(email: string, password: string) {
+  const { data, error } = await supabase.auth.signInWithPassword({
+    email,
+    password,
+  });
+  if (error) {
+    console.log("Error logging in:", error);
+  } else {
+    navigateTo("/");
+    console.log("Logged in:", data);
+  }
+}
 </script>
 
 <template>
   <div class="flex justify-center items-center w-1/2 mx-auto p-4">
-     <Card class="">
-        <CardHeader class="space-y-1">
-      <CardTitle class="text-2xl">
-        Login
-      </CardTitle>
-      <CardDescription>
-        Enter your account details to login
-      </CardDescription>
-    </CardHeader>
-    <CardContent class="grid gap-4">
-      <div class="grid grid-cols-2 gap-6">
-        <Button variant="outline">
-          <GitHubIcon class="mr-2 h-4 w-4" />
-          Github
-        </Button>
-        <Button variant="outline">
-          <svg role="img" viewBox="0 0 24 24" class="mr-2 h-4 w-4">
-            <path
-              fill="currentColor"
-              d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+    <Card>
+      <CardHeader class="space-y-1">
+        <CardTitle class="text-2xl">Login</CardTitle>
+        <CardDescription>Enter your account details to login</CardDescription>
+      </CardHeader>
+      <CardContent class="grid gap-4">
+        <div class="grid grid-cols-2 gap-6">
+          <Button variant="outline">
+            <!-- <GitHubIcon class="mr-2 h-4 w-4" /> -->
+            Github
+          </Button>
+          <Button variant="outline">
+            <!-- <GoogleIcon class="mr-2 h-4 w-4" /> -->
+            Google
+          </Button>
+        </div>
+        <div class="relative">
+          <div class="absolute inset-0 flex items-center">
+            <span class="w-full border-t" />
+          </div>
+          <div class="relative flex justify-center text-xs uppercase">
+            <span class="bg-background px-2 text-muted-foreground">
+              Or continue with
+            </span>
+          </div>
+        </div>
+        <form @submit.prevent="loginHandler(loginEmail, loginPassword)">
+          <div class="grid gap-2">
+            <Label for="email">Email</Label>
+            <Input
+              id="email"
+              type="email"
+              placeholder="m@example.com"
+              v-model="loginEmail"
             />
-          </svg>
-          Google
-        </Button>
-      </div>
-      <div class="relative">
-        <div class="absolute inset-0 flex items-center">
-          <span class="w-full border-t" />
-        </div>
-        <div class="relative flex justify-center text-xs uppercase">
-          <span class="bg-background px-2 text-muted-foreground">
-            Or continue with
-          </span>
-        </div>
-      </div>
-      <div class="grid gap-2">
-        <Label for="email">Email</Label>
-        <Input id="email" type="email" placeholder="m@example.com" />
-      </div>
-      <div class="grid gap-2">
-        <Label for="password">Password</Label>
-        <Input id="password" type="password" />
-      </div>
-    </CardContent>
-    <CardFooter>
-      <Button class="w-full">
-        Create account
-      </Button>
-    </CardFooter>
-  </Card> </div>
+          </div>
+          <div class="grid gap-2">
+            <Label for="password">Password</Label>
+            <Input id="password" type="password" v-model="loginPassword" />
+          </div>
+          <CardFooter class="p-2 mt-2">
+            <Button class="w-full" type="submit">Log in</Button>
+          </CardFooter>
+        </form>
+      </CardContent>
+    </Card>
+  </div>
 </template>
--- pages/login.vue
+++ pages/login.vue
@@ -1,23 +1,26 @@
 <script setup lang="ts">
-import CreateAccountForm from '~/components/LoginSite/CreateAccountForm.vue';
-import LoginForm from '~/components/LoginSite/LoginForm.vue';
-import { Button } from '~/components/ui/button';
-
+import CreateAccountForm from "~/components/LoginSite/CreateAccountForm.vue";
+import LoginForm from "~/components/LoginSite/LoginForm.vue";
+import { Button } from "~/components/ui/button";
+definePageMeta({
+  middleware: ["auth"],
+});
 const isLogin = ref(true);
-  </script>
-  
+</script>
+
 <template>
   <div>
     <div class="flex mx-2 justify-center p-4">
-      <Button variant="outline" class="mx-2" @click="isLogin=true">Log In</Button>
-      <Button variant="outline" class="mx-2" @click="isLogin=false">Create an account</Button>
-
+      <Button variant="outline" class="mx-2" @click="isLogin = true"
+        >Log In</Button
+      >
+      <Button variant="outline" class="mx-2" @click="isLogin = false"
+        >Create an account</Button
+      >
     </div>
-    <LoginForm v-if="isLogin"/>
-    <CreateAccountForm v-else/>
-</div>
+    <LoginForm v-if="isLogin" />
+    <CreateAccountForm v-else />
+  </div>
 </template>
 
-
-<style scoped>
-</style>
+<style scoped></style>
