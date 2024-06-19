ccb6aa6546ec3bfb0d656388102d710127bd5a07
Marco Pianaroli <marco.pianaroli96@gmail.com>
2024-06-19 09:13:49 +0200

    style: uses shadcn on index

--- components/CreateLink.vue
+++ components/CreateLink.vue
@@ -1,8 +1,9 @@
 <script setup lang="ts">
-import { ref } from 'vue';
-import { v4 as uuidv4 } from 'uuid';
-import type { Row } from '~/types';
-
+import { ref } from "vue";
+import { v4 as uuidv4 } from "uuid";
+import type { Row } from "~/types";
+import { Input } from "~/components/ui/input";
+import { Button } from "~/components/ui/button";
 const supabase = useSupabaseClient();
 const user_id = useSupabaseUser().value?.id;
 
@@ -17,14 +18,25 @@ function isValidUrl(url: string): boolean {
     return false;
   }
 }
-function checkUser () {
-  if(user_id === undefined || user_id === null) {
+function checkUser() {
+  if (user_id === undefined || user_id === null) {
     alert("User is not logged in.");
     return;
   }
 }
-const shortenLink = async () => {
-  checkUser()
+
+function generateQRCode(url: string) {
+  console.log("qr");
+  fetch(`https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${url}`)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href;
+    });
+}
+const shortenLink = async (type: "link" | "qr") => {
+  checkUser();
   if (!isValidUrl(long_url.value)) {
     return;
   }
@@ -37,7 +49,11 @@ const shortenLink = async () => {
     usage_count: 0,
   };
 
-  const { error } = await supabase.from("shortened_urls").insert([newUrl.value]);
+  const { error } = await supabase
+    .from("shortened_urls")
+    .insert([newUrl.value]);
+
+  generateQRCode(long_url.value);
 
   if (error) {
     console.error("Error creating shortened URL:", error);
@@ -50,16 +66,19 @@ const shortenLink = async () => {
 <template>
   <div class="relative">
     <main class="flex justify-center">
-      <form @submit.prevent="shortenLink" class="mx-auto ">
-        <input
+      <form @submit.prevent="" class="mx-auto">
+        <Input
           type="text"
           v-model="long_url"
           placeholder="Enter URL"
-          class="p-2 border border-gray-300 rounded-md mb-4"
+          class="m-2"
         />
-        <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded-md">
+        <Button @click="shortenLink('link')" type="submit" class="mx-2">
           Shorten
-        </button>
+        </Button>
+        <Button @click="shortenLink('qr')" type="submit" class="mx-2"
+          >Shorten + QR</Button
+        >
       </form>
     </main>
 
@@ -72,28 +91,40 @@ const shortenLink = async () => {
               <TableHead>Short URL</TableHead>
               <TableHead>Creation Date</TableHead>
               <TableHead>Clicks</TableHead>
-              <TableHead>Actions</TableHead>
+              <TableHead>QR</TableHead>
             </TableRow>
-            </TableHeader>
-            <TableBody>
-              <TableRow>
-                <TableCell class="font-medium w-2/4">
-                  <NuxtLink :to="newUrl.long_url" target="_blank">{{ newUrl.long_url }}</NuxtLink>
-                </TableCell>
-                <TableCell>
-                  <NuxtLink :to="newUrl.short_url" target="_blank">{{ newUrl.short_url }}</NuxtLink>
-                </TableCell>
-                <TableCell>{{ new Date(newUrl.creation_date).toLocaleDateString() }}</TableCell>
-                <TableCell class="text-right">
-                  {{ newUrl.usage_count }}
-                </TableCell>
-              </TableRow>
-            </TableBody>
+          </TableHeader>
+          <TableBody>
+            <TableRow>
+              <TableCell class="font-medium w-2/4">
+                <NuxtLink :to="newUrl.long_url" target="_blank">{{
+                  newUrl.long_url
+                }}</NuxtLink>
+              </TableCell>
+              <TableCell>
+                <NuxtLink :to="newUrl.short_url" target="_blank">{{
+                  newUrl.short_url
+                }}</NuxtLink>
+              </TableCell>
+              <TableCell>{{
+                new Date(newUrl.creation_date).toLocaleDateString()
+              }}</TableCell>
+              <TableCell class="text-right">
+                {{ newUrl.usage_count }}
+              </TableCell>
+              <TableCell>
+                <img
+                  :src="generateQRCode(newUrl.long_url)"
+                  alt="QR Code"
+                  class="w-8 h-8"
+                />
+              </TableCell>
+            </TableRow>
+          </TableBody>
         </Table>
       </div>
     </section>
   </div>
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
