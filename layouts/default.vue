89fa29306d8b63d3b502870c7bd035e7437201d3
Marco Pianaroli <marco.pianaroli96@gmail.com>
2024-06-21 09:33:19 +0200

    refactor: modifies navbar links and accesibility

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,13 +3,11 @@ import { ref } from "vue";
 
 const supabase = useSupabaseClient();
 const user = useSupabaseUser();
-
 const menuOpen = ref(false);
 
 function toggleMenu() {
   menuOpen.value = !menuOpen.value;
 }
-
 function logoutHandler() {
   supabase.auth
     .signOut()
@@ -21,9 +19,6 @@ function logoutHandler() {
     });
 }
 
-function loginHandler() {
-  navigateTo("/login"); // Redirect to login page or handle login modal
-}
 </script>
 <template>
   <div>
@@ -34,83 +29,40 @@ function loginHandler() {
             <!-- Mobile menu button -->
             <button
               class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              @click="toggleMenu"
-            >
+              @click="toggleMenu">
               <span class="sr-only">Open main menu</span>
-              <svg
-                class="block h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M4 6h16M4 12h16m-7 6h7"
-                />
+              <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7" />
               </svg>
-              <svg
-                class="hidden h-6 w-6"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M6 18L18 6M6 6l12 12"
-                />
+              <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                stroke="currentColor" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
               </svg>
             </button>
           </div>
           <div class="flex-shrink-0 mx-12">
-            <img
-              class="h-8 w-auto"
-              src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-              alt="Workflow"
-            >
+            <img class="h-8 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
           </div>
           <div class="hidden sm:block sm:ml-6">
-            <div
-              class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-center"
-            >
+            <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-center">
               <div v-if="user" class="flex space-x-4">
-                <NuxtLink
-                  to="/dashboard"
-                  class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >Dashboard</NuxtLink
-                >
-                <NuxtLink
-                  to="/create"
-                  class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
-                  >Create</NuxtLink
-                >
+                <NuxtLink to="/dashboard"
+                  class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  Dashboard</NuxtLink>
+                <NuxtLink to="/create"
+                  class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                  Shorten</NuxtLink>
               </div>
             </div>
           </div>
-          <div
-            class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0"
-          >
-            <button
-              v-if="user"
+          <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+            <button v-if="user"
               class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-              @click="logoutHandler"
-            >
+              @click="logoutHandler">
               <span>Log out</span>
             </button>
-            <button
-              v-else
-              class="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
-              @click="loginHandler"
-            >
-              <span>Log in</span>
-            </button>
+
           </div>
         </div>
       </div>
@@ -118,21 +70,17 @@ function loginHandler() {
       <!-- Mobile menu, show/hide based on menu state. -->
       <div v-if="menuOpen" id="mobile-menu" class="sm:hidden">
         <div v-if="user" class="px-2 pt-2 pb-3 space-y-1">
-          <NuxtLink
-            to="/dashboard"
-            class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-            >Dashboard</NuxtLink
-          >
-          <NuxtLink
-            to="/create"
-            class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
-            >Create</NuxtLink
-          >
+          <NuxtLink to="/dashboard"
+            class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+            Dashboard</NuxtLink>
+          <NuxtLink to="/create"
+            class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">
+            Shorten</NuxtLink>
         </div>
       </div>
     </nav>
     <main>
-      <slot/>
+      <slot />
     </main>
   </div>
 </template>
